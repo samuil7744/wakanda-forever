7db2117a7a510b6481c235c9d74d1214d026f1fe
samuil7744 <119361748+samuil7744@users.noreply.github.com>
2022-11-28 16:14:36 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+sprites.onOverlap(SpriteKind.Guard, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    Namor.setPosition(148, 2)
+})
+info.onScore(20, function () {
+    Namor.destroy()
+    scene.setBackgroundImage(assets.image`boston-bridge`)
+    game.over(true)
+    effects.confetti.startScreenEffect()
+    game.showLongText("We are safe", DialogLayout.Bottom)
+    Okoye.sayText("WAKANDA FOREVER", 5000, false)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    Namor.setPosition(148, 2)
+})
+let Okoye: Sprite = null
+let Namor: Sprite = null
+game.showLongText("Help Shuri, Okoye and Riri escape Namor", DialogLayout.Full)
+info.setLife(2)
+scene.setBackgroundImage(assets.image`wakanda`)
+let Shuri = sprites.create(assets.image`shuri`, SpriteKind.Player)
+controller.moveSprite(Shuri)
+Shuri.setStayInScreen(true)
+Namor = sprites.create(assets.image`namor`, SpriteKind.Enemy)
+Namor.setPosition(148, 2)
+Namor.follow(Shuri, 30)
+Okoye = sprites.create(assets.image`okoye`, SpriteKind.Guard)
+let Riri = sprites.create(assets.image`riri`, SpriteKind.Guard)
+controller.moveSprite(Riri, 34, -53)
+controller.moveSprite(Okoye, -68, -58)
